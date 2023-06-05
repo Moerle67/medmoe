c9cd126d1cf1b953e697e34c9f61fe06c5d832af
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-06-05 13:17:53 +0200

    Modell

--- medmoeapp1/admin.py
+++ medmoeapp1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+admin.site.register(Kontakt)
+admin.site.register(Medikament)
+admin.site.register(Bestellung)
+admin.site.register(Ueberweisung)
--- medmoeapp1/models.py
+++ medmoeapp1/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Kontakt(models.Model):
-    name = models.CharField("Name", max_length=50)
-    zeile1 = models.CharField("1. Adresszeile (Name)", max_length=50, primary_key=True)
-    zeile2 = models.CharField("2. Adresszeile (Straße)", max_length=50)
-    zeile3 = models.CharField("3. Adresszeile (PLZ Ort)", max_length=50)
-    zeile4 = models.CharField("4. Adresszeile", max_length=50, blank=True)
-    zeile5 = models.CharField("5. Adresszeile", max_length=50, blank=True)
-    zeile6 = models.CharField("6. Adresszeile", max_length=50, blank=True)
-    email = models.CharField("E-Mail", max_length=50, blank=True)
-    telefon = models.CharField("E-Telefon", max_length=50, blank=True)
+    name = models.CharField(verbose_name="Name", max_length=50)
+    zeile1 = models.CharField(verbose_name="1. Adresszeile (Name)", max_length=50, primary_key=True)
+    zeile2 = models.CharField(verbose_name="2. Adresszeile (Straße)", max_length=50)
+    zeile3 = models.CharField(verbose_name="3. Adresszeile (PLZ Ort)", max_length=50)
+    zeile4 = models.CharField(verbose_name="4. Adresszeile", max_length=50, blank=True)
+    zeile5 = models.CharField(verbose_name="5. Adresszeile", max_length=50, blank=True)
+    zeile6 = models.CharField(verbose_name="6. Adresszeile", max_length=50, blank=True)
+    email = models.CharField(verbose_name="E-Mail", max_length=50, blank=True)
+    telefon = models.CharField(verbose_name="E-Telefon", max_length=50, blank=True)
     
     class Meta:
         verbose_name = "Kontakt"
-        verbose_name_plural = "Kontakts"
+        verbose_name_plural = "Kontakte"
 
     def __str__(self):
         return self.name
@@ -24,19 +24,56 @@ class Kontakt(models.Model):
         return reverse("Kontakt_detail", kwargs={"pk": self.pk})
 
 class Medikament(models.Model):
-    bezeichnung = models.CharField("Bezeichnung", max_length=50)
-    details = models.CharField("Details", max_length=100)
-    menge = models.IntegerField("Menge in Packung")
-    einnahme = models.IntegerField("Regelmäßige Einnahme (Tag)")
-    letze_bestellung = models.DateField("Letzte Bestellung", auto_now=False, auto_now_add=True)
-    letzte_lieferung = models.DateField("Letzte Lieferung", auto_now=False, auto_now_add=False, blank=True)
+    bezeichnung = models.CharField(verbose_name="Bezeichnung", max_length=50)
+    details = models.CharField(verbose_name="Details", max_length=100)
+    beschreibung = models.TextField(verbose_name="Beschreibung", blank=True)
+    menge = models.IntegerField(verbose_name="Menge in Packung")
+    einnahme = models.IntegerField(verbose_name="Regelmäßige Einnahme (Tag)")
+    letze_bestellung = models.DateField(verbose_name="Letzte Bestellung", auto_now=False, auto_now_add=True)
 
     class Meta:
         verbose_name = "Medikament"
         verbose_name_plural = "Medikamente"
 
     def __str__(self):
-        return self.bezeichnung
+        return f"{self.bezeichnung} - {self.beschreibung}"
 
     def get_absolute_url(self):
         return reverse("Medikament_detail", kwargs={"pk": self.pk})
+
+class Ueberweisung(models.Model):
+    bezeichnung = models.CharField(verbose_name="Bezeichnung", max_length=50)
+    details = models.CharField(verbose_name="Details", max_length=100)
+    termin = models.DateTimeField(verbose_name="Termin", auto_now=False, auto_now_add=False)
+
+    class Meta:
+        verbose_name = "Ueberweisung"
+        verbose_name_plural = "Ueberweisungen"
+
+    def __str__(self):
+        return f"{self.bezeichnung} ({self.termin})"
+
+    def get_absolute_url(self):
+        return reverse("Ueberweisung_detail", kwargs={"pk": self.pk})
+
+
+class Bestellung(models.Model):
+    bezeichnung = models.CharField(verbose_name="Bezeichnung", max_length=50)
+    bestell_datum = models.DateField(verbose_name="Bestelldatum", auto_now=False, auto_now_add=True)
+    kunde = models.ForeignKey(Kontakt, verbose_name="Kunde", on_delete=models.RESTRICT, related_name="Kunde")
+    arzt = models.ForeignKey(Kontakt, verbose_name="Arzt", on_delete=models.RESTRICT, related_name="Arzt")
+    bemerkung = models.TextField("Bemerkung", blank=True)
+    medikamente = models.ManyToManyField(Medikament, verbose_name="Medikamente", blank=True)
+    ueberweisung = models.ManyToManyField(Ueberweisung, verbose_name="Überweisungen", blank=True)
+    
+    class Meta:
+        verbose_name = "Bestellung"
+        verbose_name_plural = "Bestellungen"
+        ordering = ['-bestell_datum']
+
+    def __str__(self):
+        return f"{self.bezeichnung} - {self.arzt} ({self.bestell_datum})"
+
+    def get_absolute_url(self):
+        return reverse("Bestellungdetail", kwargs={"pk": self.pk})
+
